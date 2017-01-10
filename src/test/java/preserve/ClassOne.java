6c6c5e4f5387c032edbc6f233368157823227c6a
sharathccs <sharath84@gmail.com>
2017-01-10 23:53:28 +0530

    added example for preserve order of execution

---
+++ src/test/java/preserve/ClassOne.java
@@ -0,0 +1,15 @@
+package preserve;
+
+import org.testng.annotations.Test;
+
+public class ClassOne {
+	@Test
+	public void firstTestCase() {
+		System.out.println("im in first test case from ClassOne Class");
+	}
+
+	@Test
+	public void secondTestCase() {
+		System.out.println("im in second test case from ClassOne Class");
+	}
+}
